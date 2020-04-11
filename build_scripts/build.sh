f52900a000da61e6408e5259c9fb922087f0c2d4
Liudmyla <GusarL@users.noreply.github.com>
2020-04-11 20:58:03 +0300

    Add build section draft

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+set -e
+echo "Source remote git repo:"
+source <(curl -s https://githubusercontent.com/GusarL/build-deploy/master/output.sh)
+
+print_info "ls -la"
+
+print_title "Node JS $(node -v)"
+print_title "NPM $(npm -v)"
+
+#Build
+print_info "Start build"
+npm install
+
