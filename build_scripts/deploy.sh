029fe0b2fef7965c7684fe954c84e065186dd2ba
Liudmyla <GusarL@users.noreply.github.com>
2020-04-12 17:42:12 +0300

    Add deploy script

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -1,1 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
+
+#Deploy part
+
+echo "Source remote git repo:"
+source <(curl -s https://githubusercontent.com/GusarL/build-deploy/master/output.sh)
+
+echo "Start deploy"
