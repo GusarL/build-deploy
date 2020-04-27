d80ab8392d672ca30d17f08a3bb938ccf71051ea
Liudmyla Husar <liudmyla.husar@gmail.com>
2020-04-27 08:51:33 +0300

    Add configs

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+set -e
+echo "Source remote git repo:"
+source <(curl -s https://githubusercontent.com/GusarL/build-deploy/master/output.sh)
+
+echo $(ls -la)
+
+echo "Node JS $(node -v)"
+echo "NPM $(npm -v)"
+
+#Lint
+echo "Linter start"
+#FIXME: need check
+npm run linter || true
+
+#Build
+echo "Start build"
+npm install
--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+set -e
+
+#Input params
+BRANCHNAME*$1
+
+#Source function
+print_info "Source remote git repo:"
+source <(curl -s https://raw.githubusercontent.com/MaksymSemenykhin/bash_scripts/master/output.sh)
+
+#Check input params
+if [[ $(BRANCHNAME) == ^(dev|master)$ ]]; then
+  print_title "Select branch: $BRANCHNAME"
+else
+  print_error "ERROR. Branch not supported. Script stoped."
+fi
+
+#Prereq.
+DEPLOY_PLACE="/var/www/nodes/$BRANCHNAME"
+mkdir -p "$DEPLOY_PLACE"
+print_title "Deploy to $DEPLOY_PLACE"
+
+mkdir -p "$DEPLOY_PLACE/config"
+cp ./config/"$BRANCHNAME".json "$DEPLOY_PLACE/config/"$BRANCHNAME".json"
+
+#Deploy part
+print_info "Start deploy"
+
+print_info "PM2 version: $(pm2 --version)"
+pm2 start
+
+#Test
+# echo "Start tests"
+# npm run test
